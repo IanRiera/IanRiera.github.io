e2a788d2b99f12a1637864d958c4663fc0ec199c
Ian Riera <ian.riera.smolinska@gmail.com>
2026-02-22 14:19:21 +0100

    Fix footer small-screen typography utilities

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
     return (
         <footer className="w-full border-t-2 border-solid border-dark
         font-medium text-sm dark:text-light dark:border-light
-        sm:font-size-[0.4rem] sm:line-height-[0.6rem]">
+        sm:text-xs sm:leading-4">
             <Layout className="py-8 flex items-center justify-between
             lg:flex-col lg:py-6">
                 <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-                <div className="flex items-right lg:py-2">
+                <div className="flex items-center lg:py-2">
                 Kudos to&nbsp;<Link href="https://devdreaming.com"
                 className="underline undirle-offset-2"
                 >CodeBucks</Link>
