9643ed27a28a15dcb280c958c454ae03003423f7
Ian Riera <ian.riera.smolinska@gmail.com>
2026-02-22 14:19:11 +0100

    Constrain mobile nav modal height for small viewports

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -118,9 +118,10 @@ const NavBar = () => {
                     <motion.div
                     initial={{ scale: 0, opacity: 0, x:"-50%", y:"-50%" }}
                     animate={{ scale: 1, opacity: 1 }}
-                    className="min-w-[70vw] flex flex-col justify-between items-center
-             fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 bg-dark/90 
-             dark:bg-light/75 rounded-lg backdrop-blur-md py-32">
+                    className="w-[min(90vw,28rem)] max-h-[85vh] overflow-y-auto
+             flex flex-col justify-between items-center fixed top-1/2 left-1/2
+             -translate-x-1/2 -translate-y-1/2 z-30 bg-dark/90 dark:bg-light/75
+             rounded-lg backdrop-blur-md py-10 px-6 sm:py-8">
                         <nav className="flex items-center flex-col justify-center">
                             <CustomMobileLink href='/' title="Home" className="" toggle={handleClick} />
                             <CustomMobileLink href='/about' title="About" className="" toggle={handleClick} />
